a42f284dbb99d5d87d5d6c77087aa0ffab0d860e
Luis Ramiro Krupoviesa <ramiro@pop-os.localdomain>
2022-01-21 13:31:56 -0300

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "../context/userContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
       <Component {...pageProps} />
     </UserProvider>
   );
